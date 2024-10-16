4e12cb3873986381a13711b0fcbbfa46b21aa70a
samuzora0129 <118950854+samuzora0129@users.noreply.github.com>
2024-10-16 14:26:48 +0900

    kadai_005_upload1

---
+++ kadai_005/bmi.js
@@ -0,0 +1,21 @@
+//変数の宣言
+let weightkg;
+
+//値の代入
+weightkg=68;
+
+//変数の宣言
+let heightm;1.7
+
+//値の代入
+heightm=1.7;
+
+//変数の宣言
+let BMI;
+
+//値の代入
+BMI=weightkg/(heightm*heightm);
+
+console.log(BMI);
+
+
