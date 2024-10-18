8bfe48dfbfa5b9f0a767f4a13dfe049108d5d836
samuzora0129 <118950854+samuzora0129@users.noreply.github.com>
2024-10-18 15:53:23 +0900

    kadai_021_upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+// output-btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById("btn");
+ 
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        const text = document.getElementById("text");  
+        text.textContent = 'ボタンをクリックしました。';  
+ }, 2000); 
+});
